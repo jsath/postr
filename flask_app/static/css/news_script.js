7815459cfffccfe1abef53ad3b3925d8586c45c6
jsath <jas224@uw.edu>
2022-09-16 23:06:17 -0700

    front_end news changes

--- flask_app/static/css/news_script.js
+++ flask_app/static/css/news_script.js
@@ -24,8 +24,8 @@ fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=11b0fa8c03c0448b98
         img2.innerHTML += `<a href=${article[2]['url']}>Link to Article</a>`
     })
     .catch(err => console.log(err) )
-    
 
+    
 let img3 = document.querySelector('.news3')
 fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=11b0fa8c03c0448b989105dcc2acecc7")
     .then(response => response.json() )
