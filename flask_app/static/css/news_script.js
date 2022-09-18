3c2fa36a824c6c9c3bab5a4a89893e65eee202fd
jsath <jas224@uw.edu>
2022-09-17 18:40:50 -0700

    loop for news page

--- flask_app/static/css/news_script.js
+++ flask_app/static/css/news_script.js
@@ -1,68 +1,27 @@
 
+const newsapi = "https://newsapi.org/v2/top-headlines?country=us&apiKey=11b0fa8c03c0448b989105dcc2acecc7"
 
-let img = document.querySelector('.news0')
-fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=11b0fa8c03c0448b989105dcc2acecc7")
-    .then(response => response.json() )
-    .then(data => {
-        let article = data['articles']
-        img.innerHTML = `<img src="${article[1]['urlToImage']}">`
-        img.innerHTML += `<p>${article[1]['title']}</p>`
-        img.innerHTML += `<a href=${article[1]['url']}>Link to Article</a>`
-    })
-    .catch(err => console.log(err) )
+async function news(){
+    const response = await fetch(newsapi);
+    const data = await response.json();
+    return data
+}
 
 
 
-
-let img2 = document.querySelector('.news2')
-fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=11b0fa8c03c0448b989105dcc2acecc7")
-    .then(response => response.json() )
-    .then(data => {
-        let article = data['articles']
-        img2.innerHTML = `<img src="${article[2]['urlToImage']}">`
-        img2.innerHTML += `<p>${article[2]['title']}</p>`
-        img2.innerHTML += `<a href=${article[2]['url']}>Link to Article</a>`
-    })
-    .catch(err => console.log(err) )
-
-    
-let img3 = document.querySelector('.news3')
-fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=11b0fa8c03c0448b989105dcc2acecc7")
-    .then(response => response.json() )
-    .then(data => {
-        let article = data['articles']
-        img3.innerHTML = `<img src="${article[3]['urlToImage']}">`
-        img3.innerHTML += `<p>${article[3]['title']}</p>`
-        img3.innerHTML += `<a href=${article[3]['url']}>Link to Article</a>`
-    })
-    .catch(err => console.log(err) )
+async function populate(){
+    const data = await news()
+    for(i = 1; i < 20; i ++){
+        console.log(data['articles'][i])
+        var content = '';
+        let img = document.querySelector(`.news${i}`)
+        content += `<img src="${data['articles'][i]['urlToImage']}">`
+        content += `<p>${data['articles'][i]['title']}</p>`
+        content += `<a href=${data['articles'][i]['url']}>Link to Article</a>`
+        content += `<p> ${data['articles'][i]['content']}<p>`
+        img.innerHTML = content;
+    }
+}
 
 
-let img4 = document.querySelector('.news4')
-fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=11b0fa8c03c0448b989105dcc2acecc7")
-    .then(response => response.json() )
-    .then(data => {
-        let article = data['articles']
-        img4.innerHTML = `<img src="${article[4]['urlToImage']}">`
-        img4.innerHTML += `<p>${article[4]['title']}</p>`
-        img4.innerHTML += `<a href=${article[4]['url']}>Link to Article</a>`
-    })
-    .catch(err => console.log(err) )
-
-
-let img5 = document.querySelector('.news5')
-fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=11b0fa8c03c0448b989105dcc2acecc7")
-    .then(response => response.json() )
-    .then(data => {
-        let article = data['articles']
-        img5.innerHTML = `<img src="${article[5]['urlToImage']}">`
-        img5.innerHTML += `<p>${article[5]['title']}</p>`
-        img5.innerHTML += `<a href=${article[5]['url']}>Link to Article</a>`
-    })
-    .catch(err => console.log(err) )
-    
-    
-
-
-
-
+populate()
